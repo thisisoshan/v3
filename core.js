7713fec1dd1b89742dab98dee2b6c3c3466bec50
Oshan Shrestha <oshan_shrestha@outlook.com>
2017-03-30 13:27:13 +0545

    Linked te retrieve photo from instagram

---
+++ core.js
@@ -0,0 +1,10 @@
+var feed = new Instafeed({
+    get: 'user',
+    userId: '3094428566',
+    clientId: 'b9402187c64f439d8be514ad1761ceed',
+    accessToken: '3094428566.1677ed0.6e3ec460b6fc4cf8bff505176f745085',
+    template:  '<a class="animation" target="_blank" href="{{link}}"><div class="img" style="background: url({{image}}) no-repeat no-repeat center center scroll; background-size: cover;"></div></a>',
+    limit: 1,
+    resolution: "standard_resolution"
+});
+feed.run();
